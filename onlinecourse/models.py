e4a2ceae780d1f80a7d2b0191525285a1f1a6a85
HalluxPollex <victor.soroka.my@gmail.com>
2023-05-08 06:23:57 -0400

    First Commit

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -95,25 +95,34 @@ class Enrollment(models.Model):
     rating = models.FloatField(default=5.0)
 
 
-# <HINT> Create a Question Model with:
-    # Used to persist question content for a course
-    # Has a One-To-Many (or Many-To-Many if you want to reuse questions) relationship with course
-    # Has a grade point for each question
-    # Has question content
-    # Other fields and methods you would like to design
+#<HINT> Create a Question Model with:
+     #Used to persist question content for a course
+     #Has a One-To-Many (or Many-To-Many if you want to reuse questions) relationship with course
+     #Has a grade point for each question
+     #Has question content
+     #Other fields and methods you would like to design
+
 #class Question(models.Model):
     # Foreign key to lesson
     # question text
     # question grade/mark
 
+class Question(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='questions')
+    grade_point = models.DecimalField(max_digits=5, decimal_places=2)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.content
+
     # <HINT> A sample model method to calculate if learner get the score of the question
-    #def is_get_score(self, selected_ids):
-    #    all_answers = self.choice_set.filter(is_correct=True).count()
-    #    selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
-    #    if all_answers == selected_correct:
-    #        return True
-    #    else:
-    #        return False
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False
 
 
 #  <HINT> Create a Choice Model with:
@@ -132,3 +141,19 @@ class Enrollment(models.Model):
 #    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
 #    choices = models.ManyToManyField(Choice)
 #    Other fields and methods you would like to design
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='choices')
+    content = models.TextField()
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.content
+
+
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE, related_name='submissions')
+    choices = models.ManyToManyField(Choice)
+
+    def __str__(self):
+        return f'Submission {self.id} for enrollment {self.enrollment.id}'
